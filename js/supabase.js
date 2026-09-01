305d4f1716cc6892c57180e4d46229a54000a855
agribusaser <agribus.aser@gmail.com>
2026-09-01 04:42:19 +0200

    Initialize Supabase client with URL and key

--- js/supabase.js
+++ js/supabase.js
@@ -1,1 +1,9 @@
+const SUPABASE_URL = "https://szfftdskbbxrufixdjhw.supabase.co";
 
+// Mets ici ta Publishable Key Supabase
+const SUPABASE_KEY = "TA_PUBLISHABLE_KEY";
+
+const supabaseClient = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+);
