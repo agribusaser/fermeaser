0b4a16c788b68db705f88d00643acb5e255e73b8
agribusaser <agribus.aser@gmail.com>
2026-09-04 11:25:52 +0200

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,11 +1,11 @@
 const SUPABASE_URL = "https://szfftdskbbxrufixdjhw.supabase.co";
 
+NEXT_PUBLIC_SUPABASE_URL=https://szfftdskbbxrufixdjhw.supabase.co
+NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY=sb_publishable_h7DMy9Vj3F2jx6oPko7ViA_pY-VhXyS
+
 const SUPABASE_KEY = "TA_VRAIE_PUBLISHABLE_KEY";
 
 const supabaseClient = window.supabase.createClient(
     SUPABASE_URL,
     SUPABASE_KEY
 );
-
-NEXT_PUBLIC_SUPABASE_URL=https://szfftdskbbxrufixdjhw.supabase.co
-NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY=sb_publishable_h7DMy9Vj3F2jx6oPko7ViA_pY-VhXyS
