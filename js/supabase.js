cbf3d5a5f7b4498cd0ab3d048a0b912661d2f0b5
agribusaser <agribus.aser@gmail.com>
2026-09-04 11:32:24 +0200

    Fix Supabase key assignment in supabase.js
    Updated Supabase key variable assignment.

--- js/supabase.js
+++ js/supabase.js
@@ -1,8 +1,6 @@
 const SUPABASE_URL = "https://szfftdskbbxrufixdjhw.supabase.co";
 
-NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY=sb_publishable_h7DMy9Vj3F2jx6oPko7ViA_pY-VhXyS
-
-const SUPABASE_KEY = "TA_VRAIE_PUBLISHABLE_KEY";
+const SUPABASE_KEY = sb_publishable_h7DMy9Vj3F2jx6oPko7ViA_pY-VhXyS;
 
 const supabaseClient = window.supabase.createClient(
     SUPABASE_URL,
