0587ea09756b58ea120f122bdd375bfd4166b625
agribusaser <agribus.aser@gmail.com>
2026-09-08 01:42:05 +0200

    Change Supabase publishable key
    Updated Supabase key to a new publishable key.

--- js/supabase.js
+++ js/supabase.js
@@ -10,7 +10,7 @@ const SUPABASE_URL =
     "https://szfftdskbbxrufixdjhw.supabase.co";
 
 const SUPABASE_KEY =
-    "TA_CLE_PUBLISHABLE_ICI";
+   sb_publishable_h7DMy9Vj3F2jx6oPko7ViA_pY-VhXyS
 
 
 // Vérification du SDK Supabase
