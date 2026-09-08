3e54c338bbb9ca64ba53b2ac3b4c22a0facbb1f6
agribusaser <agribus.aser@gmail.com>
2026-09-08 20:50:11 +0200

    Update permissions.js

--- js/permissions.js
+++ js/permissions.js
@@ -251,7 +251,9 @@ window.aPermission = aPermission;
 window.obtenirRoleERP = obtenirRoleERP;
 window.obtenirUtilisateurERP = obtenirUtilisateurERP;
 window.initialiserPermissionsERP = initialiserPermissionsERP;
+window.appliquerPermissionsMenuERP = appliquerPermissionsMenuERP;
 
 document.addEventListener("DOMContentLoaded", function () {
     initialiserPermissionsERP();
+  
 });
