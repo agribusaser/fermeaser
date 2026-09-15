f81fdecdc10f80194c3649d27f3852ec1bb24017
agribusaser <agribus.aser@gmail.com>
2026-09-15 14:32:05 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -11,7 +11,7 @@
    CONFIGURATION
 ================================================== */
 
-const CACHE_VERSION = "ferme-asher-v3";
+const CACHE_VERSION = "ferme-asher-v4";
 
 const CACHE_APP =
     `${CACHE_VERSION}-app`;
